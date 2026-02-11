4593f2121b1706f7e0e46558f4edf3ea5ed5ceef
Muhamad-Fahad01 <muhammedfahad5466@gmail.com>
2026-02-11 15:16:02 +0530

    Refactor order button event handling
    Updated event listener to prevent default action without opening modal.

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-const orderButtons = document.querySelectorAll(".order-btn");
-
-orderButtons.forEach(function(button){
-    button.addEventListener("click", function(event){
-        event.preventDefault();
-
-        // Bootstrap modal open
-        const orderModal = new bootstrap.Modal(document.getElementById('orderModal'));
-        orderModal.show();
-    });
-});
+const cartButtons = document.querySelectorAll(".order-btn");
+                    cartButtons.forEach(button => {
+                    button.addEventListener("click", function (event) {
+                    event.preventDefault();
+                    });
+                });
